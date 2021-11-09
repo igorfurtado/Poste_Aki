9c38997174587457a718fe9c0147b6e2ec643b63
Igor Furtado Targino <70289587+igorfurtado@users.noreply.github.com>
2021-11-09 16:03:36 -0300

    Cors API implementation
    Permitindo que requisições de outro domínio apontem para o meu servidor (localhost) através da implementação de uma whiteList.
    
    É necessária a seguinte instalação: npm install cors.

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const cors = require('cors');
 const posts = require("../model/posts");
+const options = {
+    origin: "http://192.168.1.17:3000"
+};
+
+router.use(cors(options));
 
 //Permitindo acesso ao corpo da requisição:
 router.use("/", express.json({ extended: true })); //Em substituição ao body-parser (descontinuado).
